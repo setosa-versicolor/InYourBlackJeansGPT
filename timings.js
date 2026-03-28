136dee6571f95cf89b89b335162e16386baabb9c
setosa-versicolor <81941874+setosa-versicolor@users.noreply.github.com>
2026-03-27 20:33:32 -0500

    Add files via upload

---
+++ timings.js
@@ -0,0 +1,7 @@
+const LYRICS = [
+{time:0,text:"In your black jeans, girl you lookin good"},
+{time:4,text:"Even when I shouldn’t, girl I wish I could"},
+{time:8,text:"Rockin your vibe, how you lookin so nice?"},
+{time:12,text:"Lookin so good that I gotta look twice"},
+{time:16,text:"Ok, maybe more than twice"}
+];
